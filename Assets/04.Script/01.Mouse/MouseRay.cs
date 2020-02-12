650ce9ef0770a05e94788e302839be2b7fad60cc
EHO712 <plasma712@gmail.com>
2020-02-12 23:27:42 +0900

    Tile 체크는 가능
    Tile 체크를 오브젝트가 있는 상태에서도 가능할지(update로 Raycast 계속 실행) 확인해보기

--- Assets/04.Script/01.Mouse/MouseRay.cs
+++ Assets/04.Script/01.Mouse/MouseRay.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class MouseRay : Singleton<MouseRay>
 {
-    RaycastHit MouseHis;// 마우스를 통해 RayCast
-
     float MaxDistance = 15.0f; // 마우스가 타일을 클릭 했을 때 무한정 멀리 있는것이 되면 안되기 때문에 설정을 해주어야함.
 
     bool bCheck; // 맵 에디터 모드일 땐 true 아닐 땐 false;
 
     public Transform target; // target은 계속 변경 가능하도록 public로 선언
 
+    public Transform SubMousePointer; // Substitute MousePointer 마우스포인터 대용. 임시객체로서 
+
 
     // 코루틴을 이용하여 특정 상황일 때만 작동을 하게 할 것!
 
     private void Update()
     {
-        // 임시로 업데이트문에 넣어둠.
-
+        // 임시로 업데이트문에 넣어둠.        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 MousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+            Debug.Log("마우스 클릭 좌표 : " + MousePos.x + " , " + MousePos.y + " , " + MousePos.z);
+        }
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mos = Input.mousePosition; // 마우스 클릭 좌표
@@ -26,15 +30,25 @@ public class MouseRay : Singleton<MouseRay>
 
             Vector3 dir = Camera.main.ScreenToWorldPoint(mos);
 
-            RaycastHit hit;
-            Debug.DrawRay(transform.position, dir , Color.red, 0.3f);
+            RaycastHit MouseHit;// 마우스를 통해 RayCast
+            Debug.DrawRay(transform.position, dir, Color.red, 0.3f);   // 디버그용도. 
 
-            if (Physics.Raycast(transform.position, dir, out hit, mos.z))
+            //Tile에서 TileInfo 스크립트에서 TA_Object false true를 체크할 것.
+            if (Physics.Raycast(transform.position, dir, out MouseHit, mos.z))
             {
-                //target.position = hit.point; // 타겟을 레이캐스트가 충돌된 곳으로 옮긴다.
-                target.position = hit.transform.GetComponent<Transform>().position;
+                /// <summary>
+                /// Tile에서 TileInfo 스크립트에서 TA_Object false true를 체크할 것.
+                /// </summary>
+                if (MouseHit.transform.GetComponent<TileInfo>().TA_Object != true)
+                {
+                    //target.position = hit.point; // 타겟을 레이캐스트가 충돌된 곳으로 옮긴다.
+                    target.position = MouseHit.transform.GetComponent<Transform>().position; //히트한 물체의 transform을 가져오기위해 히트포지션의 컴포넌트를 불러와 이용.
+                    MouseHit.transform.GetComponent<TileInfo>().TA_Object = true;
+                }
             }
         }
     }
+
+
 }
 
---
+++ Assets/04.Script/02.Tile/TileInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInfo : MonoBehaviour
+{
+    /// <summary>
+    /// TA_Object -> There is an Object .
+    /// 타일위에 오브젝트가 있는지 판단하는 구조. 
+    /// false 는 타일위에 오브젝트가 없다. true 는 타일위에 오브젝트가 있다.
+    /// 여기서 오브젝트는 지형물이거나 생성 물체로, 유닛과 같은것은 포함하지 않는다.
+    /// </summary>
+    public bool TA_Object = false; 
+
+
+}
