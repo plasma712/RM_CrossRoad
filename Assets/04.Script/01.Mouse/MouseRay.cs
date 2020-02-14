ce23cae4d5e47679ee202692de78c68e34439448
plasma712 <plasma712@gmail.com>
2020-02-14 19:12:30 +0900

    마우스 RayCast쪽 함수 분할 작업 및 UI UX 디자인
    앞으로 해야할 것
    
    데이터저장방식 및 불러오기 + UI UX 디자인 생각

--- Assets/04.Script/01.Mouse/MouseRay.cs
+++ Assets/04.Script/01.Mouse/MouseRay.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum State
+{
+    NONE, CREATE
+}
+
+
 public class MouseRay : Singleton<MouseRay>
 {
+
     // float MaxDistance = 15.0f; // 마우스가 타일을 클릭 했을 때 무한정 멀리 있는것이 되면 안되기 때문에 설정을 해주어야함.
 
     bool bCheck; // 맵 에디터 모드일 땐 true 아닐 땐 false;
@@ -12,22 +19,88 @@ public class MouseRay : Singleton<MouseRay>
 
     public Transform SubMousePointer; // Substitute MousePointer 마우스포인터 대용. 임시객체로서 
 
-    Vector3 CurMousePostion; // 현재 마우스위치와 비교하기 위해 만든 임시벡터
+    Vector3 CurMousePostion;      // 현재 마우스위치와 비교하기 위해 만든 임시벡터
+
+    int iState = (int)State.NONE; // enum state를 통해 값 변경할 예정. 초기값은 NONE인 0을 준다.
 
     private void Start()
     {
-        // start에서 현재 마우스 위치를 넣어준다.
+        //// start에서 현재 마우스 위치를 넣어준다.
         Vector3 CurMousePostion = Input.mousePosition;
     }
 
 
-
-    // 코루틴을 이용하여 특정 상황일 때만 작동을 하게 할 것!
+    public void StateChange(int _iStateNumber)
+    {
+        switch (_iStateNumber) // iState 값에 따라서 함수 사용유무 결정.
+        {
+            case 0:
+                iState = (int)State.NONE;
+                break;
+            case 1:
+                iState = (int)State.CREATE;
+                break;
+        }
+    }
 
     private void Update()
     {
-        // 임시로 업데이트문에 넣어둠.                      
+        switch (iState) // iState 값에 따라서 함수 사용유무 결정.
+        {
+            case 0:
+                break;
+            case 1:
+                ObjectDraft();
+                break;
+        }
 
+    }
+
+    #region 마우스 클릭시, 타일에 건물 설치 유무 체크
+
+    void ObjectCreate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mos = Input.mousePosition; // 마우스 클릭 좌표
+            mos.z = Camera.main.farClipPlane;  // 카메라가 바라보고 있는 방향으로 변경
+            Vector3 dir = Camera.main.ScreenToWorldPoint(mos);
+
+            RaycastHit MouseHit;// 마우스를 통해 RayCast
+            Debug.DrawRay(transform.position, dir, Color.red, 0.3f);   // 디버그용도. 
+            CurMousePostion = Input.mousePosition;
+
+            //Tile에서 TileInfo 스크립트에서 TA_Object false true를 체크할 것.
+            if (Physics.Raycast(transform.position, dir, out MouseHit, mos.z))
+            {
+                // 마우스 왼쪽 버튼 눌렀을 때
+                if (Input.GetMouseButton(0))
+                {
+                    /// <summary>
+                    /// Tile에서 TileInfo 스크립트에서 TA_Object false true를 체크할 것.
+                    /// </summary>
+                    if (MouseHit.transform.GetComponent<TileInfo>().TA_Object != true)
+                    {
+                        //target.position = hit.point; // 타겟을 레이캐스트가 충돌된 곳으로 옮긴다.
+                        target.position = MouseHit.transform.GetComponent<Transform>().position; //히트한 물체의 transform을 가져오기위해 히트포지션의 컴포넌트를 불러와 이용.
+                        MouseHit.transform.GetComponent<TileInfo>().TA_Object = true;
+                    }
+                }
+                // 마우스 우측 버튼 눌렀을 때
+                if (Input.GetMouseButtonUp(2))
+                {
+
+                }
+            }
+        }
+
+    }
+
+    #endregion
+
+    #region MouseRay를 통하여 오브젝트를 미리 배치해봄.
+    void ObjectDraft()
+    {
         if (Input.mousePosition != CurMousePostion)
         {
             //Debug.Log("마우스 현재 좌표 : " + CurMousePostion.x + " , " + CurMousePostion.y + " , " + CurMousePostion.z);
@@ -53,37 +126,10 @@ public class MouseRay : Singleton<MouseRay>
                 }
             }
         }
-
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 mos = Input.mousePosition; // 마우스 클릭 좌표
-            mos.z = Camera.main.farClipPlane;  // 카메라가 바라보고 있는 방향으로 변경
-            Vector3 dir = Camera.main.ScreenToWorldPoint(mos);
-
-            RaycastHit MouseHit;// 마우스를 통해 RayCast
-            Debug.DrawRay(transform.position, dir, Color.red, 0.3f);   // 디버그용도. 
-            CurMousePostion = Input.mousePosition;
-
-            //Tile에서 TileInfo 스크립트에서 TA_Object false true를 체크할 것.
-            if (Physics.Raycast(transform.position, dir, out MouseHit, mos.z))
-            {
-                if (Input.GetMouseButton(0))
-                {
-                    /// <summary>
-                    /// Tile에서 TileInfo 스크립트에서 TA_Object false true를 체크할 것.
-                    /// </summary>
-                    if (MouseHit.transform.GetComponent<TileInfo>().TA_Object != true)
-                    {
-                        //target.position = hit.point; // 타겟을 레이캐스트가 충돌된 곳으로 옮긴다.
-                        target.position = MouseHit.transform.GetComponent<Transform>().position; //히트한 물체의 transform을 가져오기위해 히트포지션의 컴포넌트를 불러와 이용.
-                        MouseHit.transform.GetComponent<TileInfo>().TA_Object = true;
-                    }
-                }
-            }
-        }
-
-
+        ObjectCreate(); // 마우스 클릭시, 타일에 건물 설치 유무
     }
+    #endregion
+
 
 
 }
